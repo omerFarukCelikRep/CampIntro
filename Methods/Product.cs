0ee4bf653ab5076bfecf29f28549daec1614e77b
ofarukcelik <ofarukcelik@outlook.com>
2021-02-11 21:49:24 +0300

    Product properties declared

--- Methods/Product.cs
+++ Methods/Product.cs
@@ -6,6 +6,9 @@ namespace Methods
 {
     public class Product
     {
-
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
     }
 }
