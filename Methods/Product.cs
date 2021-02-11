18bc3733bfcb06c77c43b9126a645b29d80a8c67
ofarukcelik <ofarukcelik@outlook.com>
2021-02-11 23:44:39 +0300

    new property added to Product

--- Methods/Product.cs
+++ Methods/Product.cs
@@ -11,6 +11,6 @@ namespace Methods
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
-
+        public int StockCount { get; set; }
     }
 }
