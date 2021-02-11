ae76ae5f36db56bbdbfed6bfcf7b8be4eb5f9ed5
ofarukcelik <ofarukcelik@outlook.com>
2021-02-11 22:00:42 +0300

    new products and product array created

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,6 +10,17 @@ namespace Methods
             //Clean Code
             //Best Practice
 
+            Product product1 = new Product();
+            product1.Name = "Elma";
+            product1.Price = 15;
+            product1.Description = "Amasya Elması";
+
+            Product product2 = new Product();
+            product2.Name = "Karpuz";
+            product2.Price = 80;
+            product2.Description = "Diyarbakır Karpuzu";
+
+            Product[] products = new Product[] { product1, product2 };
         }
     }
 }
