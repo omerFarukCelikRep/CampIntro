45b7457b2fbe110f88ff3732f5c151aefa47b6bc
ofarukcelik <ofarukcelik@outlook.com>
2021-02-11 19:23:24 +0300

    Creted 3 course object and added in a course array

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -6,7 +6,22 @@ namespace ClassIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Course course1 = new Course();
+            course1.CourseName = "C#";
+            course1.Teacher = "Engin Demiroğ";
+            course1.Rating = 80;
+
+            Course course2 = new Course();
+            course2.CourseName = "Java";
+            course2.Teacher = "Kerem Varış";
+            course2.Rating = 65;
+
+            Course course3 = new Course();
+            course3.CourseName = "Python";
+            course3.Teacher = "Berkay Bilgin";
+            course3.Rating = 90;
+
+            Course[] courses = new Course[] { course1, course2, course3 };
         }
     }
 }
