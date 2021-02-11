a806e8774696fd8a8f16f7c460cda1cf02698ba1
ofarukcelik <ofarukcelik@outlook.com>
2021-02-11 23:06:23 +0300

    Summation method declared

--- Mathematics/Operations.cs
+++ Mathematics/Operations.cs
@@ -6,6 +6,11 @@ namespace Mathematics
 {
     public class Operations
     {
-
+        //İki sayının toplamını ekrana yazdıran metot
+        public void Summation(int number1, int number2)
+        {
+            int result = number1 + number2;
+            Console.WriteLine("Result = " + result);
+        }
     }
 }
