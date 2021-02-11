c0a1468cb2d9843aa3b9f0226babcc7496bc7052
ofarukcelik <ofarukcelik@outlook.com>
2021-02-11 21:39:25 +0300

    Methods Project added

---
+++ Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do Not Repeat Yourself
+            //Clean Code
+            //Best Practice
+
+        }
+    }
+}
