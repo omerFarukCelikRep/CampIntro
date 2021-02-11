24d8a047e88bdc9243de4f05036277bef398dd50
ofarukcelik <ofarukcelik@outlook.com>
2021-02-11 19:17:50 +0300

    Course properties declared

---
+++ ClassIntro/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassIntro
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public string Teacher { get; set; }
+        public int Rating { get; set; }
+    }
+}
