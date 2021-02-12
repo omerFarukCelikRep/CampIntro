418f0c91e5859db40fc1b8a84027e1c04402cb92
ofarukcelik <ofarukcelik@outlook.com>
2021-02-13 00:45:06 +0300

    ProductManager class created

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        /*
+         * CRUD
+         * ----------
+         * C => Create
+         * R => Read
+         * U => Update
+         * D => Delete 
+         */
+
+        
+    }
+}
