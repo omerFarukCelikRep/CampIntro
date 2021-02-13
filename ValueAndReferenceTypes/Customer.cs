4828f80be476d5376fa42c8c4ea1e6ec6a5facdc
ofarukcelik <ofarukcelik@outlook.com>
2021-02-14 02:07:22 +0300

    Person, Customer and Employee classes created. Customer and Employee classes inherited from Person class. Properties declared

---
+++ ValueAndReferenceTypes/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValueAndReferenceTypes
+{
+    public class Customer : Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+}
---
+++ ValueAndReferenceTypes/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValueAndReferenceTypes
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
