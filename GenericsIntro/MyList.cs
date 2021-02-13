4ba6782044037094067c1a5ce9b04b4a41fd115d
ofarukcelik <ofarukcelik@outlook.com>
2021-02-13 22:29:02 +0300

    Generic MyList class created

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    public class MyList<T>
+    {
+        public void Add(T item)
+        {
+
+        }
+    }
+}
