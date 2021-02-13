79ca6078d20ce675c7ee05025aa89f3fae231612
ofarukcelik <ofarukcelik@outlook.com>
2021-02-14 02:17:55 +0300

    PersonManager class and Add method created

---
+++ ValueAndReferenceTypes/PersonManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValueAndReferenceTypes
+{
+    public class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
