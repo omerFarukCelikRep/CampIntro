d541f5fb5a049afe604bc2d97911311bbd637562
ofarukcelik <ofarukcelik@outlook.com>
2021-02-14 16:59:36 +0300

    Customer class and properties created

---
+++ OOP2/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public int CustomerNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string IdentityNumber { get; set; }
+        public string TaxNumber { get; set; }
+    }
+}
