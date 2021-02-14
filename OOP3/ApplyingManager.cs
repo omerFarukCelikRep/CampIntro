84dbfda99692dc04228e52b7b8945208462a2b5f
ofarukcelik <ofarukcelik@outlook.com>
2021-02-14 19:39:14 +0300

    ApplyingManager class created , Apply and Inform methods declared.

---
+++ OOP3/ApplyingManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    public class ApplyingManager
+    {
+        public void Apply(ICreditManager creditManager)
+        {
+            //Başvuran bilgilerini değerlendirme
+            creditManager.Calculate();
+
+        }
+
+        public void Inform(List<ICreditManager> credits)
+        {
+            foreach (ICreditManager credit in credits)
+            {
+                credit.Calculate();
+            }
+        }
+    }
+}
