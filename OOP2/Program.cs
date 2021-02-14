ee7289913597def8eb1b86b4e92b8ac75f9824a4
ofarukcelik <ofarukcelik@outlook.com>
2021-02-14 18:59:19 +0300

    examples in program.cs

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -7,14 +7,30 @@ namespace OOP2
         static void Main(string[] args)
         {
             //SOLID
-
-            Customer customer1 = new Customer();
+            //Bireysel Müşteri
+            IndividualCustomer customer1 = new IndividualCustomer();
             customer1.ID = 1;
+            customer1.CustomerNumber = "12346";
             customer1.FirstName = "Ömer Faruk";
             customer1.LastName = "Çelik";
-            customer1.IdentityNumber = "1111111111";
-            customer1.CustomerNumber = "65651";
-            customer1.CompanyName = "?";
+            customer1.IdentityNumber = "1341546";
+
+            //Kurumsal Müşteri
+            CorporateCustomer customer2 = new CorporateCustomer();
+            customer2.ID = 2;
+            customer2.CustomerNumber = "34651";
+            customer2.CompanyName = "Kodlama.io";
+            customer2.TaxNumber = "54613246546";
+
+            Customer customer3 = new IndividualCustomer();
+            Customer customer4 = new CorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+
         }
     }
 }
