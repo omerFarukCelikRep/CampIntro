499e03563f5a611ce77d68e0caa26e77b33f767d
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 01:51:13 +0300

    CraftCreditManager created and ICreditManager methods  implemented.

---
+++ OOP3/CraftCreditManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    public class CraftCreditManager : ICreditManager
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("Esnaf Kredisi Hesaplandı");
+        }
+
+        public void DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
