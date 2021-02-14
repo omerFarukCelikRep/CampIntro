4126ca7e8f210fbcc8f20a623baaa215ad5c3ba6
ofarukcelik <ofarukcelik@outlook.com>
2021-02-14 18:06:10 +0300

    CustomerManager class and Add method created

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    public class CustomerManager
+    {
+        //Business Classes
+        public void Add(Customer customer)
+        {
+            //if (customer.)
+            //{
+            //}
+
+
+        }
+    }
+}
