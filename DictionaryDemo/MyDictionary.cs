8d93b1f0f427c434a1baa6ba35964bd9f4f03bfe
ofarukcelik <ofarukcelik@outlook.com>
2021-02-14 16:41:47 +0300

    Properties and Add method created

--- DictionaryDemo/MyDictionary.cs
+++ DictionaryDemo/MyDictionary.cs
@@ -6,6 +6,23 @@ namespace DictionaryDemo
 {
     public class MyDictionary<TKey, TValue>
     {
-        
+        TKey[] keyItems;
+        TValue[] valueItems;
+        public TKey[] Key { get { return keyItems; } }
+        public TValue[] Value { get { return valueItems; } }
+        public int Length { get { return keyItems.Length; } }
+        public MyDictionary()
+        {
+            keyItems = new TKey[0];
+            valueItems = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            Array.Resize<TKey>(ref keyItems, Length + 1);
+            Array.Resize<TValue>(ref valueItems, Length + 1);
+
+            keyItems[Length - 1] = key;
+            valueItems[Length - 1] = value;
+        }
     }
 }
