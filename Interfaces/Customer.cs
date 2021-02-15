0f2570f87a153d7ce3401ae6fab78d9c300e5e42
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 17:42:30 +0300

    new property added

--- Interfaces/Customer.cs
+++ Interfaces/Customer.cs
@@ -9,5 +9,6 @@ namespace Interfaces
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
     }
 }
