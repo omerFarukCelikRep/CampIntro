aaf13384e0f6647ea8dcaf61c55f3a001dc46dd8
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 21:10:17 +0300

    MySqlCustomerDal created and methods implemented

---
+++ Interfaces/MySqlCustomerDal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    class MySqlCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("MySql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("MySql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("MySql Updated");
+        }
+    }
+}
