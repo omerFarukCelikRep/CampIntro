fb61d3a57253cb1f97fbbb9fd60e0d82aab2d540
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 17:44:46 +0300

    PersonManager class and Add method created

---
+++ Interfaces/PersonManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
