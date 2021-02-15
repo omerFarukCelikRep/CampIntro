dad5c51e1b4a9bf49c888f2c69ea88f80edf2a33
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 20:59:33 +0300

    SqlServer and OracleCustomerDal classes created and methods implemented from ICustomerDal

---
+++ Interfaces/OracleCustomerDal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated");
+        }
+    }
+}
---
+++ Interfaces/SqlServerCustomerDal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated");
+        }
+    }
+}
