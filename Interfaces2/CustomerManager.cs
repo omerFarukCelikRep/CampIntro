b5465bdf9c248a651a43b208f82dc3c11e01a92c
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 22:53:59 +0300

    CustomerManager and EmployeeManager classes created and implemented from IPersonManager

---
+++ Interfaces2/CustomerManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces2
+{
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //Müşteri Ekleme Kodları
+            Console.WriteLine("Müşteri Eklendi");
+        }
+    }
+}
---
+++ Interfaces2/EmployeeManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces2
+{
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //Personel Ekleme Kodları
+            Console.WriteLine("Personel Eklendi");
+        }
+    }
+}
