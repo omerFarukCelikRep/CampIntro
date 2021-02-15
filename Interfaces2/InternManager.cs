8e0fb8cf6ec6bedf15add5133e9ef7a0f289d9a1
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 23:08:16 +0300

    InternManager class created and implemented from IPersonManager

---
+++ Interfaces2/InternManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces2
+{
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Güncellendi");
+        }
+    }
+}
