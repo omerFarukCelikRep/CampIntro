f5e69c785fe160d34adb01da7ee046a8dd64fe03
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 17:50:06 +0300

    new customer, student created and added with personManager

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -6,7 +6,16 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PersonManager personManager = new PersonManager();
+            personManager.Add(new Customer { ID = 1, FirstName = "Ömer Faruk", LastName = "Çelik", Address = "İstanbul" });
+
+            Student student = new Student();
+            student.ID = 3;
+            student.FirstName = "Ali";
+            student.LastName = "Veli";
+            student.Departmant = "Software Engineering";
+
+            personManager.Add(student);
         }
     }
 }
