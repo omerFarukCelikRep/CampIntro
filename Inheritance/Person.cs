7b7c99e3ccaa88502e7ddd8fd16bd58d5af057f7
ofarukcelik <ofarukcelik@outlook.com>
2021-02-15 21:44:55 +0300

    Person class and properties created

---
+++ Inheritance/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
