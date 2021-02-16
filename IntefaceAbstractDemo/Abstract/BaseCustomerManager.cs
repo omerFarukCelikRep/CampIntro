606a3bff195b3a45a453c5b67926bb3237cf6489
ofarukcelik <ofarukcelik@outlook.com>
2021-02-16 15:59:03 +0300

    BaseCustomerManager class created

---
+++ IntefaceAbstractDemo/Abstract/BaseCustomerManager.cs
@@ -0,0 +1,15 @@
+﻿using IntefaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntefaceAbstractDemo.Abstract
+{
+    public class BaseCustomerManager : ICustomerService
+    {
+        public void Save(Customer customer)
+        {
+            Console.WriteLine("Saved to Database : " + customer.FirstName);
+        }
+    }
+}
