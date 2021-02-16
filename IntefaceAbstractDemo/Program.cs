9851839cf10b9c6332ce9f81a1f711df4cbdd5d0
ofarukcelik <ofarukcelik@outlook.com>
2021-02-16 15:41:15 +0300

    InterfaceAbstract Demo project Created

---
+++ IntefaceAbstractDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntefaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
