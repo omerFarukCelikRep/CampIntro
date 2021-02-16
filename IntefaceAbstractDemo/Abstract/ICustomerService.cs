14ebaa730f63753e6615c4b7d908e92fd017908c
ofarukcelik <ofarukcelik@outlook.com>
2021-02-16 15:55:21 +0300

    ICustomerService add method created

--- IntefaceAbstractDemo/Abstract/ICustomerService.cs
+++ IntefaceAbstractDemo/Abstract/ICustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntefaceAbstractDemo.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,6 @@ namespace IntefaceAbstractDemo.Abstract
 {
     public interface ICustomerService
     {
+        void Save(Customer customer);
     }
 }
