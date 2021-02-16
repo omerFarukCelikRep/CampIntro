d0da84001d5cde535e6c7a299021f185bff48e20
ofarukcelik <ofarukcelik@outlook.com>
2021-02-16 17:30:39 +0300

    MernisServiceAdapter and Connected service edited

--- IntefaceAbstractDemo/Adapters/MernisServiceAdapter.cs
+++ IntefaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using IntefaceAbstractDemo.Abstract;
+using IntefaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace IntefaceAbstractDemo.Adapters
 {
-    public class MernisServiceAdapter
+    public class MernisServiceAdapter : ICustomerCheckService
     {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody(Convert.ToInt64(customer.NationalityID), customer.FirstName, customer.LastName, customer.DateOfBirth.Year))).Result.Body.TCKimlikNoDogrulaResult;
+        }
     }
 }
