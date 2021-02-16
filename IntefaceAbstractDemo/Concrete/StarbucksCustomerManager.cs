26b47f6ba57ce22e5e2efcf08dbbb8b67dbc329c
ofarukcelik <ofarukcelik@outlook.com>
2021-02-16 16:26:58 +0300

    name fixed

---
+++ IntefaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using IntefaceAbstractDemo.Abstract;
+using IntefaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntefaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager : BaseCustomerManager
+    {
+        private ICustomerCheckService _customerCheckService;
+
+        public StarbucksCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+
+        public override void Save(Customer customer)
+        {
+            CheckIfRealPerson(customer);
+
+            base.Save(customer);
+        }
+
+        private void CheckIfRealPerson(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
