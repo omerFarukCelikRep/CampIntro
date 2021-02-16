d9b1521254c8de65190de413274d1e48c009930f
ofarukcelik <ofarukcelik@outlook.com>
2021-02-16 17:41:04 +0300

    Mernis service adapted to NeroCustomerManager

--- IntefaceAbstractDemo/Concrete/NeroCustomerManager.cs
+++ IntefaceAbstractDemo/Concrete/NeroCustomerManager.cs
@@ -8,6 +8,23 @@ namespace IntefaceAbstractDemo.Concrete
 {
     public class NeroCustomerManager : BaseCustomerManager
     {
-        
+        private ICustomerCheckService _customerCheckService;
+
+        public NeroCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+
+        public override void Save(Customer customer)
+        {
+            if (_customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.Save(customer); 
+            }
+            else
+            {
+                throw new Exception("Not a Valid Exception");
+            }
+        }
     }
 }
