13aa129220c1fc2b426727938062bad1a09dd104
ofarukcelik <ofarukcelik@outlook.com>
2021-02-18 01:06:45 +0300

    UserValidationService interface and method created and UserValidationManager implemented from it

---
+++ GameProject/Abstract/IUserValidationService.cs
@@ -0,0 +1,12 @@
+﻿using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Adapters
+{
+    public interface IUserValidationService
+    {
+        bool Validate(Gamer gamer);
+    }
+}
---
+++ GameProject/Adapters/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Adapters
+{
+    public class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.DateOfBirth.Year == 1990 && gamer.FirstName == "Ömer Faruk" && gamer.LastName == "Çelik" && gamer.IdentityNumber == 12345679801)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
