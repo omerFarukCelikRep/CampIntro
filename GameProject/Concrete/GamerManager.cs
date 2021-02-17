3090919d9eb9ec37ebedb5f387536d91763735b5
ofarukcelik <ofarukcelik@outlook.com>
2021-02-18 00:57:51 +0300

    GamerManager implemented from IGamerService

---
+++ GameProject/Concrete/GamerManager.cs
@@ -0,0 +1,26 @@
+﻿using GameProject.Abstract;
+using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Concrete
+{
+    public class GamerManager : IGamerService
+    {
+        public void Add(Gamer gamer)
+        {
+            Console.WriteLine(gamer.FirstName + " " + gamer.LastName + " Added");
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine(gamer.FirstName + " " + gamer.LastName + " Deleted");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine(gamer.FirstName + " " + gamer.LastName + " Updated");
+        }
+    }
+}
