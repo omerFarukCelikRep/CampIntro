e39675a43eb240bff707a3621f27775b7efc2561
ofarukcelik <ofarukcelik@outlook.com>
2021-02-18 01:18:36 +0300

    new validation system class added and implemented from IUserValidationService

---
+++ GameProject/Adapters/NewEStateUserValidationManager.cs
@@ -0,0 +1,15 @@
+﻿using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Adapters
+{
+    public class NewEStateUserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            return true;
+        }
+    }
+}
