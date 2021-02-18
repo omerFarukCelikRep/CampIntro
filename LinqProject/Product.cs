95ca109029a11aa743d2605fc13a5eff7a40a80a
ofarukcelik <ofarukcelik@outlook.com>
2021-02-18 19:53:11 +0300

    Product and Category classes and properties created

---
+++ LinqProject/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqProject
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public int CategoryID { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
