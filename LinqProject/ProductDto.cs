0fb05f686c3cccf6349c9b567095dd481d8b7938
ofarukcelik <ofarukcelik@outlook.com>
2021-02-20 01:03:43 +0300

    CategoryName property added

--- LinqProject/ProductDto.cs
+++ LinqProject/ProductDto.cs
@@ -7,6 +7,7 @@ namespace LinqProject
     public class ProductDto
     {
         public int ProductID { get; set; }
+        public string CategoryName { get; set; }
         public string ProductName { get; set; }
         public decimal UnitPrice { get; set; }
     }
